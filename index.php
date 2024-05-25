82055729b49780c6a7f168f8aee385daac88eae4
Julian Lima <julianlimalbs@gmail.com>
2024-05-25 08:51:03 -0300

    First commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Auth Project</title>
+</head>
+
+<body>
+  <div><span>You already has an account?</span> <a href="/login.php">Login</a></div>
+  <div><span>Don't have an account?<span> <a href="/signup.php">Signup</a></div>
+</body>
+
+</html>
